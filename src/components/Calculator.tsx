d5939eaf41d8f4685a2dc3cb6f09ae4ff264dbf9
Vitalii Hrytsenko <vitalik.gricenko77@gmail.com>
2024-06-13 01:09:02 +0300

    add some calculator functionality

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,113 +1,145 @@
+'use client'
 
-import Image from 'next/image'
-import Ingredient from '@components/Ingredient'
+import { useState } from 'react';
+import Image, { StaticImageData } from 'next/image';
+import Ingredient from '@components/Ingredient';
+import calculator_bg from '@images/calculator/calculator-bg.svg';
+import kcal_icon from '@images/calculator/kcal.svg';
+import min_icon from '@images/calculator/min.svg';
+import gram_icon from '@images/calculator/gram.svg';
+import bun_base from '@images/burger/bun-base-shadow.png';
 
-import calculator_bg from '@images/calculator/calculator-bg.svg'
+// Інгредієнти
+import ingredient_cutlet from '@images/ingridients/cutlet.png';
+import ingredient_mayo from '@images/ingridients/mayo.png';
+import ingredient_onion from '@images/ingridients/onion.png';
+import ingredient_tomato from '@images/ingridients/tomato.png';
+import ingredient_cucumber from '@images/ingridients/cucumber.png';
+import ingredient_cheese from '@images/ingridients/cheese.png';
+import ingredient_salad from '@images/ingridients/salad.png';
+import ingredient_bun from '@images/ingridients/bun.png';
 
-import kcal_icon from '@images/calculator/kcal.svg'
-import min_icon from '@images/calculator/min.svg'
-import gram_icon from '@images/calculator/gram.svg'
-
-import ketchup_gift from '@images/calculator/ketchup.png'
-
-import bun_base from '@images/burger/bun-base-shadow.png'
-
-// ingridients
-import ingredient_cutlet from '@images/ingridients/cutlet.png'
-import ingredient_mayo from '@images/ingridients/mayo.png'
-import ingredient_onion from '@images/ingridients/onion.png'
-import ingredient_tomato from '@images/ingridients/tomato.png'
-import ingredient_cucumber from '@images/ingridients/cucumber.png'
-import ingredient_cheese from '@images/ingridients/cheese.png'
-import ingredient_salad from '@images/ingridients/salad.png'
-import ingredient_bun from '@images/ingridients/bun.png'
-
+// Бургер зображення
+import burger_topBun from '@images/burger/bun-top.png';
+import burger_middleBun from '@images/burger/bun-middle.png';
+import burger_cucumber from '@images/burger/cucumber.png';
+import burger_tomato from '@images/burger/tomato.png';
+import burger_cutlet from '@images/burger/cutlet.png';
+import burger_onion from '@images/burger/onion.png';
+import burger_cheese from '@images/burger/cheese.png';
+import burger_salad from '@images/burger/salad.png';
+import burger_mayo from '@images/burger/mayo.png';
 
 const ingredients = [
-  {id: 1, name: "Cutlet", image: ingredient_cutlet, kcal: 12, time: 6, gram: 7, price: 2},
-  {id: 2, name: "Mayo", image: ingredient_mayo, kcal: 12, time: 6, gram: 7, price: 0.2},
-  {id: 3, name: "Onion", image: ingredient_onion, kcal: 12, time: 6, gram: 7, price: 0.2,},
-  {id: 4, name: "Tomato", image: ingredient_tomato, kcal: 12, time: 6, gram: 7, price: 0.2},
-  {id: 5, name: "Cucumber", image: ingredient_cucumber, kcal: 8, time: 5, gram: 4, price: 0.2},
-  {id: 6, name: "Cheese", image: ingredient_cheese, kcal: 12, time: 6, gram: 7, price: 1},
-  {id: 7, name: "Salad", image: ingredient_salad, kcal: 12, time: 6, gram: 7, price: 1},
-  {id: 8, name: "Bun", image: ingredient_bun, kcal: 12, time: 6, gram: 7, price: 1},
-]
+  { id: 1, name: 'Cutlet', image: ingredient_cutlet, kcal: 12, time: 6, gram: 7, price: 2, burgerImage: burger_cutlet, bottom: -70 },
+  { id: 2, name: 'Mayo', image: ingredient_mayo, kcal: 12, time: 6, gram: 7, price: 0.2, burgerImage: burger_mayo, bottom: -70},
+  { id: 2, name: 'Onion', image: ingredient_onion, kcal: 12, time: 6, gram: 7, price: 0.2, burgerImage: burger_onion, bottom: -50 },
+  { id: 3, name: 'Tomato', image: ingredient_tomato, kcal: 12, time: 6, gram: 7, price: 0.2, burgerImage: burger_tomato, bottom: -105 },
+  { id: 4, name: 'Cucumber', image: ingredient_cucumber, kcal: 8, time: 5, gram: 4, price: 0.2, burgerImage: burger_cucumber, bottom: -145 },
+  { id: 5, name: 'Cheese', image: ingredient_cheese, kcal: 12, time: 6, gram: 7, price: 1, burgerImage: burger_cheese, bottom: -150 },
+  { id: 6, name: 'Salad', image: ingredient_salad, kcal: 12, time: 6, gram: 7, price: 1, burgerImage: burger_salad, bottom: -85 },
+  { id: 7, name: 'Bun', image: ingredient_bun, kcal: 12, time: 6, gram: 7, price: 1, burgerImage: burger_middleBun, bottom: -80 },
+];
+
+interface IngredientProps {
+  id: number;
+  name: string;
+  image: StaticImageData;
+  kcal: number;
+  time: number;
+  gram: number;
+  price: number;
+  burgerImage: StaticImageData;
+  bottom?: number;
+}
+
 
 function Calculator() {
+  const [burgerIngredients, setBurgerIngredients] = useState<IngredientProps[]>([
+
+  ]);
+
+  const addIngredientToBurger = (ingredient: IngredientProps) => {
+    setBurgerIngredients((prevIngredients) => [...prevIngredients, ingredient,]);
+  };
+
   return (
     <section id="calculator" className="calculator pb-[92px]">
       <div className="container">
-        {/* text, burger, summary */}
         <div className="flex justify-center">
-          {/* text */}
-          <div className="mt-[164px] text-7xl font-medium text-base_black mb-4">Make <br /> Your Burger</div>
-
-          {/* burger */}
+          <div className="mt-[164px] text-7xl font-medium text-base_black mb-4">
+            Make <br /> Your Burger
+          </div>
           <div className="ml-14 mr-10 relative">
-            <Image src={calculator_bg} alt="bg"/>
+            <Image src={calculator_bg} alt="bg" />
+            <div className="burger flex flex-col justify-center items-center absolute bottom-0 left-4">
+              
+              <Image src={burger_topBun} className={`-mb-10 z-50 ${burgerIngredients.length > 2 ? "opacity-100" : "opacity-0"}`} alt="top" />
 
-            <div className="burger absolute bottom-0 left-4">
+                <div className="flex justify-center items-center flex-col-reverse">
+                  {burgerIngredients.map((ingredient, index) => {
+                  // style={{ bottom: `${index * 10}px` }}
+                  let mb;
+                  if(ingredient.bottom && index !== 0) {
+                    mb = ingredient.bottom 
+                  } else {
+                    mb = -25
+                  }
+                  if(index == 0) {
+                    mb = 50 
+                  }
 
-              <Image src={bun_base} alt='base'/>
+                  return (
+                    <div key={index} className={`w-full`} style={
+                      { marginBottom: `${mb}px`, zIndex: `${index + 1}` }
+                    }>
+                      <Image src={ingredient.burgerImage} alt={ingredient.name} />
+                    </div>
+                  )
+                  })}
+                </div>
+              <Image className='absolute bottom-0' src={bun_base} alt="base" />
             </div>
           </div>
-
-          {/* summary */}
           <div className="mt-[143px]">
             <div className="text-base_black text-4xl mb-6">Summary</div>
-            {/* burger info (kcal, min, g) */}
             <div className="flex items-center">
               <div className="flex items-center bg-[#F5F5FF] px-4 py-3 rounded-full mr-3">
-                <Image className='mr-2' src={kcal_icon} alt="kcal"/>
+                <Image className="mr-2" src={kcal_icon} alt="kcal" />
                 50 kcal
               </div>
               <div className="flex items-center bg-[#F5F5FF] px-4 py-3 rounded-full mr-3">
-                <Image className='mr-2' src={min_icon} alt="min"/>
+                <Image className="mr-2" src={min_icon} alt="min" />
                 0 min
               </div>
               <div className="flex items-center bg-[#F5F5FF] px-4 py-3 rounded-full">
-                <Image className='mr-2' src={gram_icon} alt="gram"/>
+                <Image className="mr-2" src={gram_icon} alt="gram" />
                 20 g
               </div>
             </div>
-            {/* ketchup */}
             <div className="mt-[74px] relative">
-              {/* price */}
               <div className="absolute text-4xl text-[#FFF] font-extrabold px-6 py-3 rounded-full bg-[#5243C2] -top-4 -left-1/2">
                 $0.00
               </div>
-              {/* not */}
-              <div className="text-base_black font-light text-2xl">Build a <span className='text-base_red font-bold'>$5</span> Burger and Get a Gift</div>
-
-              {/* gift */}
-              {/* <div className="">
-                <span className='text-4xl text-base_red font-extrabold mb-2'>+ Gift</span>
-                <div className="text-2xl text-base_black font-normal mb-7 ml-7">Tomato Ketchup <span className='ml-3 text-[#949BA6]'>0.2 oz</span></div>
-                <Image src={ketchup_gift} alt="ketchup" width={172} className='ml-[162px]' />
-              </div> */}
-
+              <div className="text-base_black font-light text-2xl">
+                Build a <span className="text-base_red font-bold">$5</span> Burger and Get a Gift
+              </div>
             </div>
           </div>
         </div>
-
-        {/* order button */}
         <div className="flex justify-center mt-4 mb-6">
-          <button 
-            className="text-[#FFF] text-4xl font-extrabold px-6 py-3 bg-[#BE2B20] rounded-full disabled:bg-[#F5F5FF]">
+          <button className="text-[#FFF] text-4xl font-extrabold px-6 py-3 bg-[#BE2B20] rounded-full disabled:bg-[#F5F5FF]">
             Order
           </button>
         </div>
-
-        {/* Ingridients */}
         <div className="flex justify-between">
           {ingredients.map((ingredient) => (
-            <Ingredient key={ingredient.id} ingridient={ingredient} />
+            <Ingredient key={ingredient.id} ingredient={ingredient} onClick={() => addIngredientToBurger(ingredient)} />
           ))}
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default Calculator
+export default Calculator;
--- src/components/Ingredient.tsx
+++ src/components/Ingredient.tsx
@@ -1,10 +1,11 @@
-'use client'
+'use client';
 
 import Image, { StaticImageData } from 'next/image';
+import { useState } from 'react';
+
 
 import minus from '@images/minus.svg'
 import plus from '@images/plus.svg'
-import { useState } from 'react';
 
 interface IngredientProps {
   id: number;
@@ -17,35 +18,41 @@ interface IngredientProps {
 }
 
 interface IngredientComponentProps {
-  ingridient: IngredientProps;
+  ingredient: IngredientProps;
+  onClick: () => void;
 }
 
-const Ingredient: React.FC<IngredientComponentProps> = ({ ingridient }) => {
+const Ingredient: React.FC<IngredientComponentProps> = ({ ingredient, onClick }) => {
+  const [count, setCount] = useState(0);
 
-  const [count, setCount] = useState(0)
-
-  // const [image, kcal, time, gram, price] = ingridient;
+  const ingredientToggle = (cont: string) => {
+    if (cont === 'minus') {
+      if (count - 1 < 0) return;
+      setCount((prev) => prev - 1);
+    } else if (cont === 'plus') {
+      if (count + 1 > 20) return;
+      setCount((prev) => prev + 1);
+    } else {
+      console.log('Error');
+    }
+  };
 
   return (
-    <div className='border border-[#F0F7FA] bg-[#FFF] rounded-[20px] w-[140px] h-[180px] flex flex-col justify-center items-center '>
+    <div
+      className="border border-[#F0F7FA] bg-[#FFF] rounded-[20px] w-[140px] h-[180px] flex flex-col justify-center items-center"
+      onClick={onClick}
+    >
       <div className="min-h-[60px] mb-3 flex items-center justify-center">
-        <Image src={ingridient.image} alt="Ingridient" className='' />
+        <Image src={ingredient.image} alt="Ingredient" />
       </div>
-      
-
-      <div className="text-sm text-base_black font-medium mb-3">
-        {ingridient.name}
-      </div>
-
+      <div className="text-sm text-base_black font-medium mb-3">{ingredient.name}</div>
       <div className="counter flex justify-center items-center">
-        <div className="cursor-pointer size-8 rounded-full bg-[#F5F5FF] flex justify-center items-center">
-          <Image src={minus} alt='Minus' />
+        <div onClick={() => ingredientToggle('minus')} className="cursor-pointer size-8 rounded-full bg-[#F5F5FF] flex justify-center items-center">
+          <Image src={minus} alt="Minus" />
         </div>
-
         <div className="text-base text-base_black font-medium mx-3">{count}</div>
-
-        <div className="cursor-pointer size-8 rounded-full bg-[#F5F5FF] flex justify-center items-center">
-          <Image src={plus} alt='plus' />
+        <div onClick={() => ingredientToggle('plus')} className="cursor-pointer size-8 rounded-full bg-[#F5F5FF] flex justify-center items-center">
+          <Image src={plus} alt="plus" />
         </div>
       </div>
     </div>
@@ -53,3 +60,5 @@ const Ingredient: React.FC<IngredientComponentProps> = ({ ingridient }) => {
 };
 
 export default Ingredient;
+
+
